f1c2d6816f0d761d667a1598395075b7cb255c13
Natasha Shpak <n.v.shpak@yandex.ru>
2022-11-27 11:08:28 +0400

    before new popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import initialCards from "./data.js";
+
 const popupElement = document.querySelector('.popup');
 const editProfileElement = document.querySelector('.profile__edit-button');
 const addCardElement = document.querySelector('.profile__add-button');
@@ -7,39 +9,11 @@ let profileElement = document.querySelector('.profile');
 let profileNameElement = profileElement.querySelector('.profile__name');
 let profileJobElement = profileElement.querySelector('.profile__job');
 const formTitleElement = formElement.querySelector('.popup__title');
-const fields = formElement.querySelectorAll('.popup__field');
-
+const inputFields = formElement.querySelectorAll('.popup__field');
 
 const cardsListElement = document.querySelector('.elements__list');
 const templateCardElement = document.querySelector('#card-template').content.querySelector('li');
 
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 function createCard(item) {
   const card = templateCardElement.cloneNode(true);
@@ -78,28 +52,28 @@ initialCards.forEach(function(item) {
 function editProfile() {
   popupElement.classList.add('popup_opened');
   formElement.name = 'edit-profile__form';
-  fields[0].classList.add('popup__field_about_name');
-  fields[0].type = 'text';
-  fields[1].classList.add('popup__field_about_job');
-  fields[1].type = 'text';
+  inputFields[0].classList.add('popup__field_about_name');
+  inputFields[0].type = 'text';
+  inputFields[1].classList.add('popup__field_about_job');
+  inputFields[1].type = 'text';
 
   formTitleElement.textContent = 'Редактировать профиль';
   formElement.querySelector('.popup__submit').textContent = 'Сохранить';
-  fields[0].value = profileNameElement.textContent;
-  fields[1].value = profileJobElement.textContent;
+  inputFields[0].value = profileNameElement.textContent;
+  inputFields[1].value = profileJobElement.textContent;
 }
 
 function addCard() {
   popupElement.classList.add('popup_opened');
   formElement.name = 'add-card__form';
-  fields[0].classList.add('popup__field_about_name');
-  fields[0].type = 'text';
-  fields[0].value = '';
-  fields[0].placeholder = 'Название';
-  fields[1].classList.add('popup__field_about_job');
-  fields[1].type = 'url';
-  fields[1].value = '';
-  fields[1].placeholder = 'Ссылка на картинку';
+  inputFields[0].classList.add('popup__field_about_name');
+  inputFields[0].type = 'text';
+  inputFields[0].value = '';
+  inputFields[0].placeholder = 'Название';
+  inputFields[1].classList.add('popup__field_about_job');
+  inputFields[1].type = 'url';
+  inputFields[1].value = '';
+  inputFields[1].placeholder = 'Ссылка на картинку';
   formTitleElement.textContent = 'Новое место';
   formElement.querySelector('.popup__submit').textContent = 'Создать';
 }
@@ -112,20 +86,20 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   const currentFormName = evt.target.closest('form').name;
   if (currentFormName === 'edit-profile__form') {
-    profileNameElement.textContent = fields[0].value;
-    profileJobElement.textContent = fields[1].value;
+    profileNameElement.textContent = inputFields[0].value;
+    profileJobElement.textContent = inputFields[1].value;
   } else if (currentFormName === 'add-card__form') {
     const newCardData = {
-      name: fields[0].value,
-      link: fields[1].value
+      name: inputFields[0].value,
+      link: inputFields[1].value
     };
-    const newCard = createCard(newCardData);
-    console.log(newCard);
-    cardsListElement.append(newCard);
+    if (newCardData['name'] && newCardData['link']) {
+      const newCard = createCard(newCardData);
+      cardsListElement.prepend(newCard);
+    }
   }
   closePopup();
 }
-
 
 
 editProfileElement.addEventListener('click', editProfile);
